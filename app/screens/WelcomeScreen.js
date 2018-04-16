0f147434bc3266ea4ac112fda43eaea9663e4e69
Girish Rawat <Emailgirishrawat@gmail.com>
2018-04-16 01:33:45 -0700

    Redesigned welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -16,79 +16,158 @@ export default class WelcomeScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.titleText}>
-                    How would you to like to start your day?
+
+                {/* Plan your Day */}
+
+                <Text style={styles.nameText}>
+                    Hi Girish
                 </Text>
-                <Button
-                    title="Movies"
-                    titleStyle={{ fontWeight: "700" }}
-                    color="blue"
-                    buttonStyle={{
-                        backgroundColor: "rgba(92, 99,216, 0)",
-                        width: 100,
-                        height: 45,
-                        borderColor: "transparent",
-                        borderWidth: 0,
-                        borderRadius: 5
-                    }}
-                    containerStyle={{ marginTop: 20 }}
-                />
-                <Button
-                    title="Beach"
-                    titleStyle={{ fontWeight: "700" }}
-                    color="blue"
-                    buttonStyle={{
-                        backgroundColor: "rgba(92, 99,216, 0)",
-                        width: 100,
-                        height: 45,
-                        borderColor: "transparent",
-                        borderWidth: 0,
-                        borderRadius: 5
-                    }}
-                    containerStyle={{ marginTop: 20 }}
-                />
-                <Button
-                    title="Food"
-                    titleStyle={{ fontWeight: "700" }}
-                    color="blue"
-                    buttonStyle={{
-                        backgroundColor: "rgba(92, 99,216, 0)",
-                        width: 100,
-                        height: 45,
-                        borderColor: "transparent",
-                        borderWidth: 0,
-                        borderRadius: 5
-                    }}
-                    containerStyle={{ marginTop: 20 }}
-                />
-                <Button
-                    title="Pizza"
-                    titleStyle={{ fontWeight: "700" }}
-                    color="blue"
-                    buttonStyle={{
-                        backgroundColor: "rgba(92, 99,216, 0)",
-                        width: 100,
-                        height: 45,
-                        borderColor: "transparent",
-                        borderWidth: 0,
-                        borderRadius: 5
-                    }}
-                    containerStyle={{ marginTop: 20 }}
-                />
-                <Button
-                    onPress={() => this.props.navigation.navigate('OptionDetail')}
-                    title="Go"
-                    titleStyle={{ fontWeight: "700" }}
-                    buttonStyle={{
-                        backgroundColor: "rgba(92, 99,216, 1)",
-                        width: 100,
-                        height: 45,
-                        borderColor: "transparent",
-                        borderWidth: 0,
-                        borderRadius: 5
-                    }}
-                    containerStyle={{ marginTop: 20 }}
-                />
+                <Text style={styles.placeText}>
+                    Welcome to San Francisco
+                </Text>
+                <Text style={styles.greetingText}>
+                    Lets start planning your weekend!
+                </Text>
+
+                <View style={styles.planningButton}>
+                    <Button
+                        onPress={() => this.props.navigation.navigate('OptionDetail')}
+                        title="Start Planning"
+                        titleStyle={{ fontWeight: "700" }}
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 1)",
+                            width: 320,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                    />
+                </View>
+
+                {/* Specific Plans */}
+
+                <Text style={styles.greetingText}>
+                    Have something specific in mind?
+                </Text>
+
+                {/* <View style={styles.planningButton}>
+                    <Button
+                        onPress={() => this.props.navigation.navigate('OptionDetail')}
+                        title="Start Planning"
+                        titleStyle={{ fontWeight: "700" }}
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 1)",
+                            width: 320,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                    />
+                </View> */}
+
+                <View style={styles.smallRow}>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.thumbnailImage}
+                            source={require('../../images/theatre.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.thumbnailImage}
+                            source={require('../../images/food.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.smallRow2}>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.thumbnailImage}
+                            source={require('../../images/beach.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.thumbnailImage}
+                            source={require('../../images/museum.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                {/* <View style={styles.buttonContainer}>
+                    <Button
+                        title="Movies"
+                        titleStyle={{ fontWeight: "700" }}
+                        color="blue"
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 0)",
+                            width: 100,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                        containerStyle={{ marginTop: 20 }}
+                    />
+                    <Button
+                        title="Beach"
+                        titleStyle={{ fontWeight: "700" }}
+                        color="blue"
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 0)",
+                            width: 100,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                        containerStyle={{ marginTop: 20 }}
+                    />
+                    <Button
+                        title="Food"
+                        titleStyle={{ fontWeight: "700" }}
+                        color="blue"
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 0)",
+                            width: 100,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                        containerStyle={{ marginTop: 20 }}
+                    />
+                    <Button
+                        title="Pizza"
+                        titleStyle={{ fontWeight: "700" }}
+                        color="blue"
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 0)",
+                            width: 100,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                        containerStyle={{ marginTop: 20 }}
+                    />
+                    <Button
+                        onPress={() => this.props.navigation.navigate('OptionDetail')}
+                        title="Go"
+                        titleStyle={{ fontWeight: "700" }}
+                        buttonStyle={{
+                            backgroundColor: "rgba(92, 99,216, 1)",
+                            width: 100,
+                            height: 45,
+                            borderColor: "transparent",
+                            borderWidth: 0,
+                            borderRadius: 5
+                        }}
+                        containerStyle={{ marginTop: 20 }}
+                    />
+                </View> */}
             </View>
         );
     }
@@ -97,12 +176,60 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+    },
+
+    placeText: {
+        fontSize: 20,
+        textAlignVertical: "center",
+        textAlign: "left",
+        marginLeft: 30,
+    },
+
+    nameText: {
+        fontSize: 40,
+        textAlignVertical: "center",
+        textAlign: "left",
+        marginLeft: 30,
+        marginTop: 30
+    },
+    greetingText: {
+        fontSize: 25,
+        textAlignVertical: "center",
+        textAlign: "left",
+        marginLeft: 30,
+        marginTop: 30
+    },
+
+    buttonContainer: {
         alignItems: 'center',
         justifyContent: 'center'
     },
-    titleText: {
-        fontSize: 30,
-        textAlignVertical: "center",
-        textAlign: "center",
+
+    smallRow: {
+        flex: 1,
+        width: '100%',
+        flexDirection: 'row',
+        marginLeft: 30,
+        marginTop: 12
+    },
+
+    smallRow2: {
+        flex: 1,
+        width: '100%',
+        flexDirection: 'row',
+        marginLeft: 30,
+        marginTop: -5
+    },
+
+    thumbnailImage: {
+        height: 145,
+        width: 145,
+        marginRight: 10
+    },
+
+    planningButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10
     }
 })
